6cd68268636c86dba36a5fd0d48e79088f3ad156
Thiago Pereira <thiagopnascimento11@gmail.com>
2025-04-17 19:21:18 -0300

    Atividade e Categoria

---
+++ src/test/java/com/thiago/evento/EventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
